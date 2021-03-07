86259b7837b7e28bd7d3d9f64c62f399589942b5
somefb <somefromb@gmail.com>
2021-03-07 17:38:19 +0300

    CheckBot. Add repeat, valid and invalid counter

--- src/userCheckBot/index.ts
+++ src/userCheckBot/index.ts
@@ -8,6 +8,9 @@ import { generateWordPairs, generateWordPairsNext } from "./dictionary";
 const rows = 4;
 const collumns = 3;
 
+// validation section here
+const expectedValidTimes = 2; // twice in the row
+const expectedInvalidTimes = 3; // total possible == twice
 //todo implement waitTimeout
 const waitTimeout = 60 * 1000; //1 minute
 
@@ -69,7 +72,7 @@ async function callbackStart(msg: Message.TextMessage, service: ITelegramService
       }
 
       if (!res.ok) {
-        await service.core.sendMessage({ chat_id: msg.chat.id, text: "Я поломался. Давай ещё раз" });
+        await service.core.sendMessage({ chat_id: msg.chat.id, text: "Я поломался. Давайте ещё раз" });
         const err = new Error("Need to repeat");
         err.name = "Repeat";
         throw err;
@@ -80,47 +83,64 @@ async function callbackStart(msg: Message.TextMessage, service: ITelegramService
 
     const ukey = { num: 1, word: "волк" };
 
-    // first pair
-    const pairs = generateWordPairs(ukey, rows * collumns);
-    const r = (await sendMessage(
-      "Выберите слово",
-      pairs.map((v) => v.one)
-    )) as Message.TextMessage;
+    let validTimes = 0;
+    let invalidTimes = 0;
+    let msgPrefix = "";
+    while (1) {
+      // first part
+      const pairs = generateWordPairs(ukey, rows * collumns);
+      const r = (await sendMessage(
+        msgPrefix + "Выберите слово",
+        pairs.map((v) => v.one)
+      )) as Message.TextMessage;
 
-    message_id = r.message_id;
+      message_id = r.message_id;
 
-    // wait for response
-    const e = await service.onGotCallbackQuery((e) => e.callback_query.message?.chat.id === chatId);
-    //todo start timer here and wait for 1 minutes
-    const gotWord = (e.result.callback_query as CallbackQuery.DataCallbackQuery)?.data;
-    const trueWordPair = gotWord && pairs.find((v) => v.one === gotWord);
-    if (!trueWordPair) {
-      await service.core.sendMessage({ chat_id: msg.chat.id, text: "Упс, я поломался. Давай ещё раз" });
-      return;
-    }
-
-    // second part
-    const nextObj = generateWordPairsNext(ukey, trueWordPair, pairs, true);
-    const r2 = await sendMessage(
-      `Выберите слово 2`,
-      nextObj.pairs.map((v) => v.two)
-    );
-
-    const e2 = await service.onGotCallbackQuery((e) => e.callback_query.message?.chat.id === chatId);
-    const gotWord2 = (e2.result.callback_query as CallbackQuery.DataCallbackQuery)?.data;
-    if (gotWord2 === nextObj.expected) {
-      await sendMessage(arrayGetRandomItem(answersExpected_1), null);
-      // todo repeat here
-    } else {
-      // todo mark as invalid after 3 times
-      if (gotWord2 === nextObj.truthy) {
-        await sendMessage(arrayGetRandomItem(answersTrue), null);
-      } else {
-        await sendMessage(arrayGetRandomItem(answersFalse), null);
+      // wait for response
+      const e = await service.onGotCallbackQuery((e) => e.callback_query.message?.chat.id === chatId);
+      //todo start timer here and wait for 1 minutes
+      const gotWord = (e.result.callback_query as CallbackQuery.DataCallbackQuery)?.data;
+      const trueWordPair = gotWord && pairs.find((v) => v.one === gotWord);
+      if (!trueWordPair) {
+        await service.core.sendMessage({ chat_id: msg.chat.id, text: "Упс, я поломался. Давайте ещё раз" });
+        return;
       }
-    }
 
-    console.warn("got result", JSON.stringify(r), r);
+      // second part
+      const nextObj = generateWordPairsNext(ukey, trueWordPair, pairs, true);
+      await sendMessage(
+        `Выберите ассоциацию`,
+        nextObj.pairs.map((v) => v.two)
+      );
+
+      const e2 = await service.onGotCallbackQuery((e) => e.callback_query.message?.chat.id === chatId);
+      const gotWord2 = (e2.result.callback_query as CallbackQuery.DataCallbackQuery)?.data;
+      if (gotWord2 === nextObj.expected) {
+        ++validTimes;
+        if (validTimes >= expectedValidTimes) {
+          await sendMessage(arrayGetRandomItem(answersExpected_2), null);
+          //todo timeout 10 sec and remove private chat
+          break;
+        } else {
+          msgPrefix = arrayGetRandomItem(answersExpected_1) + ". Давайте повторим. ";
+        }
+      } else {
+        validTimes = 0;
+        ++invalidTimes;
+        if (gotWord2 === nextObj.truthy) {
+          await sendMessage(arrayGetRandomItem(answersTrue), null);
+        } else {
+          await sendMessage(arrayGetRandomItem(answersFalse), null);
+        }
+        if (invalidTimes >= expectedInvalidTimes) {
+          // todo mark as invalid ???
+          // todo mark as locked - requires more strict validation
+          // todo timeout 10 sec and remove private chat
+          break;
+        }
+      }
+      console.warn("got result", JSON.stringify(r), r);
+    }
   } catch (err) {
     if (err.name !== "repeat") {
       console.error("CheckBot error. " + err);
