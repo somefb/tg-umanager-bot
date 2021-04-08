1af8a54b8ddad6f64c0374bfd899fbe4eed75ad0
somefb <somefromb@gmail.com>
2021-04-08 14:43:12 +0300

    Commands. CheckAll. Finish logic (need improvements)

--- src/commands/checkAll.ts
+++ src/commands/checkAll.ts
@@ -1,5 +1,9 @@
 import ErrorCancelled from "../errorCancelled";
-import { CommandRepeatBehavior, EventTypeEnum, MyBotCommand, NewCallbackQuery } from "../types";
+import dateToPastTime from "../helpers/dateToPastTime";
+import Repo from "../repo";
+import { CommandRepeatBehavior, EventTypeEnum, IBotContext, MyBotCommand, NewCallbackQuery } from "../types";
+import { CheckBot } from "../userCheckBot";
+import UserItem from "../userItem";
 import { MyBotCommandTypes } from "./botCommandTypes";
 
 const CheckAll: MyBotCommand = {
@@ -9,92 +13,154 @@ const CheckAll: MyBotCommand = {
   description: "проверить участников",
   repeatBehavior: CommandRepeatBehavior.restart,
   callback: async (ctx) => {
+    const ctxTask = ctx.service.initContext(ctx.chatId, "_cntAll", ctx.initMessage, ctx.user);
+    ctxTask.callCommand(countAllTask);
+
+    ctx.singleMessageMode = true;
     ctx.setTimeout(0);
 
-    const r1 = await ctx.service.core.getChatMembersCount({ chat_id: ctx.chatId });
-    // WARN: excluded Me from count
-    let membersCnt = (r1.ok && r1.result) || 0;
-    const r2 = await ctx.service.core.getChatAdministrators({ chat_id: ctx.chatId });
-    const admins = (r2.ok && r2.result) || [];
+    let throttle: NodeJS.Timeout | null = null;
+    const report = (forceNow = false) => {
+      if (throttle) {
+        return Promise.resolve();
+      }
+      return new Promise<void>((resolve) => {
+        throttle = setTimeout(
+          async () => {
+            throttle = null;
+            const arr = ["Статус пользователей\n"];
+            Object.keys(ctx.chat.members).forEach((key) => {
+              const m = ctx.chat.members[key];
+              if (m.isBot) {
+                return;
+              }
+              const user = Repo.getUser(m.id);
+              let status: string;
+              //todo show icons for status
+              if (!user) {
+                //todo wait for registration or not ?
+                status = "не зарегестрирован";
+              } else if (user.isLocked) {
+                //todo show instructions
+                status = `заблокирован ${dateToPastTime(user.validationDate)}`;
+              } else if (user.isValid) {
+                status = `проверен ${dateToPastTime(user.validationDate)}`;
+              } else {
+                //todo show CheckBot is blocked
+                status = `ожидание... Было ${dateToPastTime(user.validationDate)}`;
+              }
 
-    admins.forEach((v) => {
-      const isMe = v.user.is_bot && v.user.username === ctx.botUserName;
-      !isMe && ctx.chat.addOrUpdateMember(v.user, !!v.is_anonymous);
+              const uLink = UserItem.ToLink(m.id, m.userName, m.firstName, m.lastName);
+              arr.push(`${uLink} - ${status}`);
+            });
+
+            await ctx.sendMessage({
+              text: arr.join("\n"),
+              parse_mode: "HTML",
+              disable_notification: true,
+            });
+
+            resolve();
+          },
+          forceNow ? 0 : 5 * 60000
+        );
+      });
+    };
+
+    await report(true);
+
+    //todo what if this command will be fired again in a short time?
+    const arr: Promise<unknown>[] = [];
+    Object.keys(ctx.chat.members).forEach((key) => {
+      const user = Repo.getUser(ctx.chat.members[key].id);
+      if (user) {
+        arr.push(CheckBot.validateUser(user).finally(report));
+      }
     });
 
-    let definedCnt = ctx.chat.calcVisibleMembersCount() + 1;
-
-    if (membersCnt > definedCnt) {
-      ctx.singleMessageMode = true;
-      const sendCount = (cnt: number) => {
-        const names = Object.keys(ctx.chat.members)
-          .map((k) => ctx.chat.members[k])
-          .filter((v) => !v.isAnonym)
-          .map((v) => (v.lastName ? v.firstName + " " + v.lastName : v.firstName))
-          .sort();
-        return ctx.sendMessage({
-          text: [`Определено ${cnt} из ${membersCnt} участников\n`, "Я", ...names].join("\n"),
-          parse_mode: "HTML",
-        });
-      };
-      await sendCount(definedCnt);
-      ctx.singleMessageMode = false;
-      const m = await ctx.sendMessage({
-        text: [
-          "Я не имею доступа к списку участников (ограничение телеграмма), однако вижу всех администраторов.",
-          "\nЕсли среди участников есть другой бот (не администратор) и его нет в списке выше - удалите и добавьте его, или временно измените ему права...",
-          "\nПомогите обнаружить вас...",
-        ].join("\n"),
-        parse_mode: "HTML",
-        reply_markup: {
-          inline_keyboard: [[{ text: "Я здесь", callback_data: "im" }]],
-        },
-      });
-      ctx.singleMessageMode = true;
-
-      // (async () => {
-      let evRef: Promise<NewCallbackQuery>;
-      ctx.chat.onChatMembersCountChanged = (inc) => {
-        membersCnt += inc;
-        const eData: Partial<NewCallbackQuery> = { data: "im" };
-        ctx.removeEvent(evRef);
-        ctx.getListener(evRef)?.resolve(eData as NewCallbackQuery);
-      };
-
-      ctx.setTimeout(12 * 60 * 60000); // 12 hours
-      try {
-        while (1) {
-          evRef = ctx.onGotEvent(EventTypeEnum.gotCallbackQuery);
-          const q = await evRef;
-          if (q.data != "im") {
-            continue;
-          }
-          q.id && (await ctx.service.core.answerCallbackQuery({ callback_query_id: q.id, text: "Спасибо" }));
-          const newCount = ctx.chat.calcVisibleMembersCount() + 1;
-
-          if (newCount >= membersCnt) {
-            ctx.deleteMessage(m.message_id);
-            break;
-          } else if (newCount !== definedCnt) {
-            definedCnt = newCount;
-            await sendCount(definedCnt);
-          }
-        }
-      } catch (err) {
-        if (!(err as ErrorCancelled).isCancelled) {
-          console.error(err);
-        }
-      }
-      // })();
-    }
-    ctx.chat.onChatMembersCountChanged = undefined;
-
-    await ctx.sendMessage({ text: "А теперь приступим к регистрации..." });
-
-    // else if (allCnt < definedAllCnt) {
-    //   console.warn("Chat: some members are removed without mine detection");
-    // }
+    await Promise.all(arr);
+    //todo notify task is finished
   },
 };
 
 export default CheckAll;
+
+async function countAllTask(ctx: IBotContext) {
+  ctx.setTimeout(0);
+
+  const r1 = await ctx.service.core.getChatMembersCount({ chat_id: ctx.chatId });
+  // WARN: excluded Me from count
+  let membersCnt = (r1.ok && r1.result) || 0;
+  const r2 = await ctx.service.core.getChatAdministrators({ chat_id: ctx.chatId });
+  const admins = (r2.ok && r2.result) || [];
+
+  admins.forEach((v) => {
+    const isMe = v.user.is_bot && v.user.username === ctx.botUserName;
+    !isMe && ctx.chat.addOrUpdateMember(v.user, !!v.is_anonymous);
+  });
+
+  let definedCnt = ctx.chat.calcVisibleMembersCount() + 1;
+
+  if (membersCnt > definedCnt) {
+    ctx.singleMessageMode = true;
+    const sendCount = (cnt: number) => {
+      const names = Object.keys(ctx.chat.members)
+        .map((k) => ctx.chat.members[k])
+        .filter((v) => !v.isAnonym)
+        .map((v) => (v.lastName ? v.firstName + " " + v.lastName : v.firstName))
+        .sort();
+      return ctx.sendMessage({
+        text: [`Определено ${cnt} из ${membersCnt} участников\n`, "Я", ...names].join("\n"),
+        parse_mode: "HTML",
+      });
+    };
+    await sendCount(definedCnt);
+    ctx.singleMessageMode = false;
+    const m = await ctx.sendMessage({
+      text: [
+        "Я не имею доступа к списку участников (ограничение телеграмма), однако вижу всех администраторов.",
+        "\nЕсли среди участников есть другой бот (не администратор) и его нет в списке выше - удалите и добавьте его, или временно измените ему права...",
+        "\nПомогите обнаружить вас...",
+      ].join("\n"),
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [[{ text: "Я здесь", callback_data: "im" }]],
+      },
+    });
+    ctx.singleMessageMode = true;
+
+    let evRef: Promise<NewCallbackQuery>;
+    ctx.chat.onChatMembersCountChanged = (inc) => {
+      membersCnt += inc;
+      const eData: Partial<NewCallbackQuery> = { data: "im" };
+      ctx.removeEvent(evRef);
+      ctx.getListener(evRef)?.resolve(eData as NewCallbackQuery);
+    };
+
+    ctx.setTimeout(12 * 60 * 60000); // 12 hours
+    try {
+      while (1) {
+        evRef = ctx.onGotEvent(EventTypeEnum.gotCallbackQuery);
+        const q = await evRef;
+        if (q.data != "im") {
+          continue;
+        }
+        q.id && (await ctx.service.core.answerCallbackQuery({ callback_query_id: q.id, text: "Спасибо" }));
+        const newCount = ctx.chat.calcVisibleMembersCount() + 1;
+
+        if (newCount >= membersCnt) {
+          ctx.deleteMessage(m.message_id);
+          break;
+        } else if (newCount !== definedCnt) {
+          definedCnt = newCount;
+          await sendCount(definedCnt);
+        }
+      }
+    } catch (err) {
+      if (!(err as ErrorCancelled).isCancelled) {
+        console.error(err);
+      }
+    }
+  }
+  ctx.chat.onChatMembersCountChanged = undefined;
+}
---
+++ src/helpers/dateToPastTime.ts
@@ -0,0 +1,41 @@
+export default function dateToPastTime(dt: number): string {
+  const diffSec = (Date.now() - dt) / 1000;
+  if (diffSec < 60) {
+    return Math.round(diffSec) + "с назад";
+  }
+
+  //less 1hour
+  if (diffSec < 3600) {
+    const diffMin = Math.floor(diffSec / 60);
+    let str = diffMin + "м ";
+    if (diffMin < 5) {
+      const leftSec = diffSec - diffMin * 60;
+      if (leftSec) {
+        str += leftSec + "с ";
+      }
+    }
+    return str + "назад";
+  }
+
+  //less 1day
+  if (diffSec < 24 * 3600) {
+    const diffHour = Math.floor(diffSec / 3600);
+    let str = diffHour + "ч ";
+    const leftMin = Math.floor(diffSec / 60 - diffHour * 60);
+    if (leftMin) {
+      str += leftMin + "м ";
+    }
+    return str + "назад";
+  }
+
+  // >= 1day
+  const diffDay = Math.floor(diffSec / (24 * 3600));
+  let str = diffDay + "д ";
+  if (diffDay <= 2) {
+    const leftHour = Math.floor(diffSec / 3600 - diffDay * 24);
+    if (leftHour) {
+      str += leftHour + "ч ";
+    }
+  }
+  return str + "назад";
+}
