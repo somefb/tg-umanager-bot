dd6bc869d265f403219d11456dfdae27014e0627
somefb <somefromb@gmail.com>
2021-04-09 18:56:09 +0300

    Todo's. Remove outdated

--- src/botContext.ts
+++ src/botContext.ts
@@ -116,8 +116,7 @@ export default class BotContext implements IBotContext {
 
     if (this.singleMessageMode && this._updateMessageId) {
       (args as Opts<"editMessageText">).message_id = this._updateMessageId;
-      //WARN: editMessage returns text-result but we don't need one
-      // todo: user can remove message by mistake. We need to create sendNew instead. Need to handle apiError
+      // WARN: user can remove message by mistake and we can't detect id
       await this.service.core.editMessageText(args as Opts<"editMessageText">);
       data = this._updateMessageData as Message.TextMessage;
     } else {
